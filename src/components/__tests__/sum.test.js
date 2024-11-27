f53721b8fe77cb34c71f0ba2be3db132e3ac43b4
TusharRajput07 <tusharrajput2002@gmail.com>
2024-11-27 21:36:27 +0530

    updated folder structure

--- src/components/__tests__/sum.test.js
+++
@@ -1,6 +0,0 @@
-import { sum } from "../Sum";
-
-test("should calculate sum of two numbers", () => {
-  const result = sum(3, 4);
-  expect(result).toBe(7);
-});
--- src/utils/constants.js
+++
@@ -1,2 +0,0 @@
-export const RES_IMAGE_URL =
-  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
