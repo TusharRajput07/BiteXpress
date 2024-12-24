b6385be2a72af1b98e964e1e9131083ad14c0554
TusharRajput07 <tusharrajput2002@gmail.com>
2024-12-24 22:32:12 +0530

    updated backend vercel configs

---
+++ backend/api/menu.js
@@ -0,0 +1,25 @@
+import fetch from "node-fetch";
+
+export default async function handler(req, res) {
+  console.log("attempting to get menu");
+
+  const { resId } = req.query;
+
+  const swiggyMenuApi =
+    "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.65420&lng=77.23730&restaurantId=" +
+    resId +
+    "&catalog_qa=undefined&submitAction=ENTER";
+
+  try {
+    const response = await fetch(swiggyMenuApi, {
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+      },
+    });
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch menu from Swiggy API" });
+  }
+}
